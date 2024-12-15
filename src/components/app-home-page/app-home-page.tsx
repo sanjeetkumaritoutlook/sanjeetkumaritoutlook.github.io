6b658591ce137d67368d85a875f9a3765e10d12a
unknown <sanjeetkumarit@outlook.com>
2024-12-15 06:10:29 +0530

    link to stencil app starter

--- src/components/app-home-page/app-home-page.tsx
+++ src/components/app-home-page/app-home-page.tsx
@@ -12,7 +12,7 @@ export class AppHome {
       <div class="app-home-page">
         <p>
           Welcome to Sanjeet Kumar's Home Page.<p>
-          </p> This website is created using Stencil App Starter and hosted on  <a href="https://pages.github.com/">GitHub Pages.</a> You can use  Stencil App Starter to build entire apps all with web components using Stencil! Check out docs on{' '}
+          </p> This website is created using <a href="https://github.com/stencil-community/stencil-app-starter">Stencil App Starter</a> and hosted on <a href="https://pages.github.com/">GitHub Pages.</a> You can use  Stencil App Starter to build entire apps all with web components using Stencil! Check out docs on{' '}
           <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
         <p><strong>Please Note:</strong> Currently, The routing to different pages is not working perfectly and you can navigate to each page, after clicking it from main Home page only. Direct URLs to each individual page is not working. </p>
